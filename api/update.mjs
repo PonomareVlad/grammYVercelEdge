334078c47814e913c7499b9b2d8d4c8bdb60c560
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-05-13 15:26:07 +0500

    Added edge-grammy package

--- api/update.mjs
+++ api/update.mjs
@@ -1,6 +1,6 @@
 import bot from "../src/bot.mjs";
 import {webhookCallback} from "grammy/web";
 
-export default webhookCallback(bot, "std/http", "throw", 29000);
-
 export const config = {runtime: "edge"};
+
+export default webhookCallback(bot, "std/http", "throw", 29000);
--- api/webhook.mjs
+++ api/webhook.mjs
@@ -1,17 +1,6 @@
 import bot from "../src/bot.mjs";
-
-const getURL = ({
-                    headers = {},
-                    path = "api/update",
-                    header = "x-forwarded-host",
-                }) => {
-    const host = headers?.get(header) || process.env.VERCEL_URL;
-    return new URL(path, `https://${host}`).href;
-}
-
-export default async ({headers}) => {
-    const result = await bot.api.setWebhook(getURL({headers}));
-    return new Response(JSON.stringify({result}));
-}
+import {setWebhook} from "edge-grammy";
 
 export const config = {runtime: "edge"};
+
+export default setWebhook(bot, "api/update");
