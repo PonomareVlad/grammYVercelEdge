50e81f1683b9b81f5ef7e7dfe0a342103942bd52
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-10-15 06:45:27 +0200

    remove conditional build

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,34 +1,17 @@
-import { bot, secretToken } from '../src/bot.mjs'
+import { bot, secretToken as secret_token } from '../src/bot.mjs'
 import { getURL } from 'vercel-grammy'
-
-const { VERCEL_ENV } = process.env
-
-// List of allowed environments
-const allowedEnvs = [
-    'production',
-    // "preview"
-]
-
-// Check bot
-await bot.init()
-
-// Exit in case of unsuitable environments
-if (!allowedEnvs.includes(VERCEL_ENV)) process.exit()
 
 // Webhook URL generation
 const url = getURL({ path: 'api/webhook' })
 
-// Webhook setup options
-const options = { secret_token: secretToken }
-
 // Installing a webhook
-if (await bot.api.setWebhook(url, options)) {
+if (await bot.api.setWebhook(url, { secret_token })) {
 
     // Checking the webhook installation
     const { url } = await bot.api.getWebhookInfo()
 
+    console.info('Secret token:', secret_token)
     console.info('Webhook set to URL:', url)
-    console.info('Secret token:', secretToken)
     console.info('Info:', bot.botInfo)
 
 }
