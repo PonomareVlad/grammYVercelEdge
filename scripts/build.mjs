e17e6718a4a88709ccb5777546058fe2e9b8bbe5
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-10-15 06:58:03 +0200

    remove vercel-grammy

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,11 +1,16 @@
 import { bot, secretToken as secret_token } from '../src/bot.mjs'
-import { getURL } from 'vercel-grammy'
+
+// Default Vercel hostnames
+export const {
+    VERCEL_URL = 'localhost',
+    VERCEL_BRANCH_URL: hostname = VERCEL_URL,
+} = process.env
 
 // Webhook URL generation
-const url = getURL({ path: 'api/webhook' })
+const url = new URL('api/webhook', `https://${hostname}`)
 
 // Installing a webhook
-if (await bot.api.setWebhook(url, { secret_token })) {
+if (await bot.api.setWebhook(url.href, { secret_token })) {
 
     // Checking the webhook installation
     const { url } = await bot.api.getWebhookInfo()
